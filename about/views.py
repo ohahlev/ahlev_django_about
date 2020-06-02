a86e5590bdd193ac434bee34cc9f3c46caf76f72
ahlev <ohahlev@gmail.com>
2020-06-02 22:03:29 +0700

    update view

--- about/views.py
+++ about/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import About
 
 def index(request):
-    about_us_list = About.objects.all()
+    abouts = About.objects.all()
     context = {
-        'about': about_us_list[0] if len(about_us_list) > 0 else []
+        'about': abouts[0] if len(abouts) > 0 else []
     }
-
     return render(request, 'about/index.html', context=context)
